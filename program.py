b72cc94c985262abeccbededcd87c71f00794db4
CHETAN KULTHE <chetangdoinfotech@gmail.com>
2020-11-10 16:20:15 +0530

    added add to group button and wallet button

--- program.py
+++ program.py
@@ -2,12 +2,15 @@ import json
 import requests
 #### My Bot ###
 import telebot
-
 from telebot import types
-
+import logging
+logger = telebot.logger
+#telebot.logger.setLevel(logging.DEBUG)
 
 TOKEN = '1287299690:AAHxBShPZa4D7Yz5HP6_x1Pe8WuU1HM5-cQ'
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+BOTNAME='MykurdishBot' 
 
 welcome_text ="""Welcome to MyTipBot, a Telegram Tipper Bot for multiple blockchains. Click the Menu icon to get started.
 
@@ -17,8 +20,17 @@ MyTipBot automatically generates a wallet address for you on multiple blockchain
 Select which wallet you would like to open by pressing the 🏦 Wallets button.
 Check your public deposit address for each blockchain by pressing the 🔤 Address button."""
 
-
 welcome_text1 ="""Click on the button below, choose the group and start tipping your friends! """
+
+wallet_text = """Binance DEX - BEP-20 BNB 
+
+Description
+Binance DEX refers to the decentralized exchange features developed on top of Binance Smart Chain.
+
+Links
+🌐 Website (https://www.binance.org/en/smartChain)
+🐦 Twitter (https://twitter.com/binance_dex) """
+
 
 #@bot.message_handler(commands=['help'])
 #def send_welcome(message):
@@ -33,7 +45,7 @@ def handle_message(message):
 	bot.reply_to(message, "HELP HERE") 
 
 
-@bot.message_handler(regexp="wallet")
+@bot.message_handler(regexp="wt")
 def handle_message(message):
 	bot.reply_to(message, message)
 
@@ -48,10 +60,37 @@ def handle_message(message):
 	#bot.send_message(message.chat.id, welcome_text)
 	bot.send_message(message.chat.id, welcome_text, reply_markup=markup)
 	inline_markup = types.InlineKeyboardMarkup()
-	inline_markup.add(types.InlineKeyboardButton(text="Add MyTipBot to your Group", url= "http://google.com", callback_data="callback_btn"))
+	inline_markup.add(types.InlineKeyboardButton(text="Add MyTipBot to your Group", url="https://telegram.me/"+BOTNAME+"?startgroup=true"))
 	bot.send_message(message.chat.id, welcome_text1, reply_markup=inline_markup)
 
 
+
+@bot.message_handler(regexp="🏦 Wallet")
+def handle_message(message):
+	myinline_markup = types.InlineKeyboardMarkup()
+	myinline_markup.add(
+		types.InlineKeyboardButton(text="🔤 Address", callback_data="callbackbnb_addr"),
+		types.InlineKeyboardButton(text="💰 Balance", callback_data="callbackbnb_bal"),
+		types.InlineKeyboardButton(text="🔑 Private Key", callback_data="callbackbnb_pk")
+	)
+
+	bot.send_message(message.chat.id, wallet_text, reply_markup=myinline_markup)
+
+
+###### CALL BACK QUERY HANDLER 
+@bot.callback_query_handler(func=lambda call: True)
+def callbackbnb_addr(call):
+	if call.data == "callbackbnb_addr":
+		print(call.data)
+
+	if call.data == "callbackbnb_bal":
+		print(call.data)
+
+        if call.data == "callbackbnb_pk":
+		print(call.data)
+
+
+###### FOR ANY OTHER NONSENSE TEXT #####    DELETE  TEXT/CHAT TEXT ######## REMAIN
 @bot.message_handler()
 def handle_message(message):
 	bot.reply_to(message, message.chat.id)
